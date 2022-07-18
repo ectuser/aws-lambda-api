c344942d1d85929563c9394fbf1cabcaaf4ba4cf
Иван Меньщиков <ivanmensikov@MacBook-Air-Ivan.local>
2022-07-18 22:02:13 +0200

    Implemented task 3

---
+++ product-service/handler.js
@@ -0,0 +1,37 @@
+'use strict';
+const fs = require('fs').promises;
+
+module.exports.getProductsList = async (event) => {
+  const data = await fs.readFile('./mocks/products.json');
+  const products = JSON.parse(data);
+  return {
+    statusCode: 200,
+    body: JSON.stringify(products),
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    }
+  };
+};
+
+module.exports.getProductsById = async (event) => {
+  const data = await fs.readFile('./mocks/products.json');
+  const products = JSON.parse(data);
+  const product = products.find((el) => el.id === paramId);
+  if (product) {
+    return {
+      statusCode: 200,
+      body: JSON.stringify(product),
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      }
+    };
+  } else {
+    return {
+      statusCode: 404,
+      body: 'Product not found',
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      }
+    };
+  }
+};
